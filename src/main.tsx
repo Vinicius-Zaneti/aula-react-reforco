e6d228c1cf36dc79b550128a25f6f6b49c31a809
Vinicius <viniciuszaneti.vz@gmail.com>
2023-08-26 16:54:31 -0300

    Subindo Header Reforco

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
+// import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+   
+      <Header />
+    
+  </React.StrictMode>,
+)
